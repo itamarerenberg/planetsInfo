567293f23b75d9a974e9b21dafb3fbddf7cc4274
itamarerenberg <59229663+itamarerenberg@users.noreply.github.com>
2022-02-21 16:06:29 +0200

    first draft for bl

---
+++ BL/BLClass.cs
@@ -0,0 +1,35 @@
+﻿using BE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    /// <summary>
+    /// logical operations
+    /// </summary>
+    public class BLClass
+    {
+        public string PoD()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PlanetInfo GetPlanetInfo(string pname)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PlanetInfo SearchByImage(string imUrl)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<PlanetInfo> qurey(DateTime from, DateTime until, float diameter=0, bool isDengerous=false)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
