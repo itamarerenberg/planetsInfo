da7e19a99e4d783e2dced9b1bb6c32e07d4eab0a
itamarerenberg <59229663+itamarerenberg@users.noreply.github.com>
2022-03-21 15:49:24 +0200

    created POD class

---
+++ BE/POD.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    /// <summary>
+    /// picture of the day
+    /// </summary>
+    public class POD
+    {
+        public string picName { get; set; }
+        public string picUrl { get; set; }
+        public string explenation { get; set; }
+        public DateTime date { get; set; }
+        public string imageCredit { get; set; }
+        public string tomorrowPic { get; set; }
+    }
+}
--- DL/DLClass.cs
+++ DL/DLClass.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BE;
 
 namespace DL
 {
-    public class Class1
+    public class DLClass
     {
+        public BE.POD getPod()
+        {
+
+        }
     }
 }
