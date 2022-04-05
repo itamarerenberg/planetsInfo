c97c2777ead127219776834a3a412e5ec3802837
itamarerenberg <59229663+itamarerenberg@users.noreply.github.com>
2022-04-05 14:12:37 +0300

    arrenged planet info class

--- BE/PlanetInfo.cs
+++ BE/PlanetInfo.cs
@@ -16,44 +16,19 @@ namespace BE
             public string self { get; set; }
         }
 
-        public class Page
-        {
-            public int size { get; set; }
-            public int total_elements { get; set; }
-            public int total_pages { get; set; }
-            public int number { get; set; }
-        }
-
-        public class Kilometers
+        public class mesures
         {
             public double estimated_diameter_min { get; set; }
             public double estimated_diameter_max { get; set; }
         }
 
-        public class Meters
-        {
-            public double estimated_diameter_min { get; set; }
-            public double estimated_diameter_max { get; set; }
-        }
-
-        public class Miles
-        {
-            public double estimated_diameter_min { get; set; }
-            public double estimated_diameter_max { get; set; }
-        }
-
-        public class Feet
-        {
-            public double estimated_diameter_min { get; set; }
-            public double estimated_diameter_max { get; set; }
-        }
 
         public class EstimatedDiameter
         {
-            public Kilometers kilometers { get; set; }
-            public Meters meters { get; set; }
-            public Miles miles { get; set; }
-            public Feet feet { get; set; }
+            public mesures kilometers { get; set; }
+            public mesures meters { get; set; }
+            public mesures miles { get; set; }
+            public mesures feet { get; set; }
         }
 
         public class RelativeVelocity
@@ -81,62 +56,23 @@ namespace BE
             public string orbiting_body { get; set; }
         }
 
-        public class OrbitClass
-        {
-            public string orbit_class_type { get; set; }
-            public string orbit_class_description { get; set; }
-            public string orbit_class_range { get; set; }
-        }
-
-        public class OrbitalData
-        {
-            public string orbit_id { get; set; }
-            public string orbit_determination_date { get; set; }
-            public string first_observation_date { get; set; }
-            public string last_observation_date { get; set; }
-            public int data_arc_in_days { get; set; }
-            public int observations_used { get; set; }
-            public string orbit_uncertainty { get; set; }
-            public string minimum_orbit_intersection { get; set; }
-            public string jupiter_tisserand_invariant { get; set; }
-            public string epoch_osculation { get; set; }
-            public string eccentricity { get; set; }
-            public string semi_major_axis { get; set; }
-            public string inclination { get; set; }
-            public string ascending_node_longitude { get; set; }
-            public string orbital_period { get; set; }
-            public string perihelion_distance { get; set; }
-            public string perihelion_argument { get; set; }
-            public string aphelion_distance { get; set; }
-            public string perihelion_time { get; set; }
-            public string mean_anomaly { get; set; }
-            public string mean_motion { get; set; }
-            public string equinox { get; set; }
-            public OrbitClass orbit_class { get; set; }
-        }
-
-
         public class NearEarthObject
         {
             public Links links { get; set; }
             public string id { get; set; }
             public string neo_reference_id { get; set; }
             public string name { get; set; }
-            public string name_limited { get; set; }
-            public string designation { get; set; }
             public string nasa_jpl_url { get; set; }
             public double absolute_magnitude_h { get; set; }
             public EstimatedDiameter estimated_diameter { get; set; }
             public bool is_potentially_hazardous_asteroid { get; set; }
             public List<CloseApproachData> close_approach_data { get; set; }
-            public OrbitalData orbital_data { get; set; }
             public bool is_sentry_object { get; set; }
         }
 
         public class Root
         {
             public Links links { get; set; }
-            public Page page { get; set; }
             public List<NearEarthObject> near_earth_objects { get; set; }
         }
 
